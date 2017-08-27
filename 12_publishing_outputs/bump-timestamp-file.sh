52f932fda16c5aad53a85b71a05deb66c5deedcc
Yun Wang <yun.wang@rea-group.com>
2017-08-27 22:16:12 +0800

    update the logic for push logic on 12

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -6,10 +6,10 @@ set -x # print commands
 git clone resource-gist updated-gist
 
 cd updated-gist
-echo $(date) > test-for-concourse
+echo $(date) > version__file
 
 git config --global user.email "640238027@qq.com"
 git config --global user.name "Concourse"
 
 git add .
-git commit -m "Bumped date"
+git commit -m "update version file"
